ea289b297be28ed2f7f093f85af6d27f681a692b
Darwin Bollepalli <darwin.bollepalli@gmail.com>
2019-10-06 09:07:10 +0530

    Imdb Episode Class

---
+++ Episode.py
@@ -0,0 +1,22 @@
+
+class Episode:
+    'IMDB Episode Class'
+    dummy=10
+    def __init__(self,url,name,number,rating,storyline,imdb_title_number):
+        self.url = url
+        self.name = name
+        self.number = number
+        self.rating = rating
+        self.storyline = storyline
+        self.imdb_title_number = imdb_title_number
+
+    def show_details(self):
+        # print(self.number+"   ["+self.url+"]")
+        print(self.number)
+        print(self.rating)
+        print("["+self.name+"]"+"  ---  ["+self.imdb_title_number+"]")
+        print(self.storyline)
+        print('-------------------------------')
+
+    def write_file(self):
+        print("incomplete")
