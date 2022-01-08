dac7536af51669e2acb2709303d20e8e2424dc6f
Darwin Bollepalli <darwin.bollepalli@gmail.com>
2022-01-07 18:55:32 -0600

    displaying crawling time, No of cores used stats

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from joblib import Parallel, delayed
 import multiprocessing
 import itertools
 import sys
-
+import timeit
 import g_search
 import Episode
 import crawl
@@ -29,17 +29,19 @@ print("------------------------------------------------------------------------"
 try:
     top_ratedlist_url = "https://www.imdb.com/search/title/?series="+title_number+"&view=simple&count=250&sort=user_rating,desc&ref_=tt_eps_rhs_sm"
 except:
-    print("selected ivalid IMDB link for tv series")
+    print("Please select valid IMDB link for tv series")
+    print("")
     sys.exit()
 # top_ratedlist_url="https://www.imdb.com/search/title/?series=tt2085059&view=simple&count=250&sort=user_rating,desc&ref_=tt_eps_rhs_sm"
 episodes_list = crawl.get_episodes_list(top_ratedlist_url)
 
-
-# print(episodes_list)
-
 # multiprocessing support
 num_cores = multiprocessing.cpu_count()
+print("Num Cores available for multiprocessing: ",num_cores)
+
+start_time = timeit.default_timer()
 top_list = Parallel(n_jobs=num_cores)(delayed(crawl.get_episode)(title) for title in episodes_list)
+end_time1 = timeit.default_timer()
 
 for title,episode in zip(episodes_list,top_list):
     episode.name = episodes_list[title]
@@ -47,10 +49,14 @@ for title,episode in zip(episodes_list,top_list):
 top_list.reverse()
 for episode in top_list:
     episode.show_details()
+end_time2 = timeit.default_timer()
 
-print("================================================")
-
-
-
-
-
+print("")
+print("===============================")
+print("===============================")
+print("Time taken for crawling: ", end_time1 - start_time, " secs")
+print("No of cores used : ", num_cores)
+print("No of Episodes : ", len(episodes_list))
+print("===============================")
+print("===============================")
+print("")
